755edfa1b50266f1f03b55e92479e1005c96d361
TerryThreatt <terry.threatt@gmail.com>
2021-12-28 16:18:27 -0600

    reverse a string solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // Solution 1
+    // return str
+    //     .split("")
+    //     .reverse()
+    //     .join("")
+
+    // Solution 2
+    // let result = '';
+
+    // for (let char of str) {
+    //     result = char + result;
+    // }
+
+    // return result
+
+    // Solution 3
+    return str.split("").reduce((reversed, char) => {
+        return char = char + reversed;
+    }, '')
+}
 
 module.exports = reverse;
