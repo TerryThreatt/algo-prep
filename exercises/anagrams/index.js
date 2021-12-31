49edb454e9aed920742bde8fafd3024502b3b8f2
TerryThreatt <terry.threatt@gmail.com>
2021-12-30 19:49:15 -0600

    solved anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    // Solution 1
+
+
+    // const aCharMap = buildCharMap(stringA);
+    // const bCharMap = buildCharMap(stringB);
+
+    // if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    //     return false;
+    // }
+
+    // for (let char in aCharMap) {
+    //     if (aCharMap[char] !== bCharMap[char]) {
+    //         return false;
+    //     }
+    // }
+
+    // return true;
+
+
+    // Solution 2
+
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function buildCharMap(str) {
+    const charMap = {};
+
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}
+
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
 
 module.exports = anagrams;
