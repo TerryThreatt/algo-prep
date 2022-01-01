d8cde30e8eb4c0f413e08252ba8e6149cc23324a
TerryThreatt <terry.threatt@gmail.com>
2021-12-31 18:05:57 -0600

    solved steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,40 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+    // Solution 1
+
+    // for (let row = 0; row < n; row++){
+    //     let stair = '';
+
+    //     for (let column = 0; column < n; column++){
+    //         if (column <= row) {
+    //             stair += '#';
+    //         } else {
+    //             stair += ' ';
+    //         }
+    //     }
+
+    //     console.log(stair);
+    // }
+
+    // Solution 2
+
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair)
+}
 
 module.exports = steps;
