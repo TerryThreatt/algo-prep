466975f44ff02d244fb55201c4e175200fd57bee
TerryThreatt <terry.threatt@gmail.com>
2022-01-04 18:54:00 -0600

    added insertFirst() and size() functions

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -3,16 +3,34 @@
 // See 'directions' document
 
 class Node {
-    constructor(data, next = null) {
-        this.data = data;
-        this.next = next;
-    }
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    const node = new Node(data, this.head);
+
+    this.head = node;
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
     }
+
+    return counter;
+  }
 }
 
 const list = new LinkedList();
