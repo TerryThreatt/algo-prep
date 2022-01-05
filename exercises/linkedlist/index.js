c7995cd3830dd3b13ace4fe695dcad0ffb5a0ad2
TerryThreatt <terry.threatt@gmail.com>
2022-01-04 18:19:39 -0600

    added a linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,20 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+}
+
+const list = new LinkedList();
+list.head = new Node(10);
 
 module.exports = { Node, LinkedList };
