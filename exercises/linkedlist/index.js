db8f3d10d0bf7a1d50fb5c06fd72a4497e6c0c6d
TerryThreatt <terry.threatt@gmail.com>
2022-01-05 19:11:22 -0600

    added getAt() and removeAt() methods

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -47,7 +47,7 @@ class LinkedList {
       if (!node.next) {
         return node;
       }
-      node = node.next
+      node = node.next;
     }
   }
 
@@ -63,36 +63,70 @@ class LinkedList {
     return (this.head = this.head.next);
   }
 
-    removeLast() {
-        if (!this.head) {
-            return;
-        }
-
-        if (!this.head.next) {
-            this.head = null;
-            return;
-        }
-
-        let previous = this.head;
-        let node = this.head.next;
-
-        while (node.next) {
-            previous = node;
-            node = node.next;
-        }
-
-        previous.next = null;
+  removeLast() {
+    if (!this.head) {
+      return;
     }
 
-    insertLast(data) {
-        const last = this.getLast();
-
-        if (last) {
-            last.next = new Node(data);
-        } else {
-            this.head = new Node(data);
-        }
+    if (!this.head.next) {
+      this.head = null;
+      return;
     }
+
+    let previous = this.head;
+    let node = this.head.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
+
+  getAt(index) {
+    if (!this.head) {
+      return null;
+    }
+
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return null;
+    }
+
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+      const prev = this.getAt(index - 1);
+      if (!prev || !prev.next) {
+          return;
+      }
+      prev.next = prev.next.next;
+  }
 }
 
 const list = new LinkedList();
