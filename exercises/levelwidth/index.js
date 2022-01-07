62700523de585cd37943a3c610c17eef39382b23
TerryThreatt <terry.threatt@gmail.com>
2022-01-06 18:01:05 -0600

    added levelWidth() function

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr = [root, 's'];
+    const counter = [0]
+
+    while (arr.length > 1) {
+        const node = arr.shift();
+
+        if (node === 's') {
+            counter.push(0);
+            arr.push('s');
+        } else {
+            arr.push(...node.children);
+            counter[counter.length - 1]++;
+        }
+    }
+
+    return counter
+}
 
 module.exports = levelWidth;
