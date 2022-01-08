54a37273fda3140d9fb0dacc23642deaa8a63785
TerryThreatt <terry.threatt@gmail.com>
2022-01-08 13:11:00 -0600

    solved mergeSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -34,8 +34,30 @@ function selectionSort(arr) {
     return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
+    ['a', 'b', 'c', 'd']
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0,center);
+    const right = arr.slice(center);
 
-function merge(left, right) {}
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    const results = [];
+
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+
+    return [...results, ...left, ...right];
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
